1fda9362a7bf1c0ff8b2b65cdc733b30443f3d2c
Artem <artembabuhkin@gmail.com>
2023-03-06 18:00:52 +0300

    Добавил текстовый файл с регулярным выражением проверяющим дату в формате dd.mm.yyyy от 1900 до 2023 года.В абстрактном классе Publishing создал проверку с помощью regex

--- Publishing/Publishing/Publishing.cpp
+++ Publishing/Publishing/Publishing.cpp
@@ -1,10 +1,8 @@
-﻿// Publishing.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <locale>
 #include <stdlib.h>
+#include <regex>
 using namespace std;
 
 class Publishing { //Abstract parent class
@@ -12,11 +10,12 @@ private:
 	string Name;
 	int Year;
 	char tmp_Year[255];
-
+	
+	
 
 public:
 	void setName() {
-		while (Name.size() < 3) {	//Line length >3
+		while (!regex_match(Name,regex("[A-Z][a-z]{1,32}"))) {
 			cout << "Enter a title > 3 characters:\n";
 			getline(cin, Name);
 		}
@@ -43,17 +42,7 @@ public:
 int main() {  							//!!!DO AN ISBN CHECK!!!
 	locale::global(std::locale(""));
 	Publishing test;
-	test.setYear();
-	cout << test.getYear();
+	test.setName();
+	cout << test.getName();
 	return 0;
 }
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
