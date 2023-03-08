007a66942b8a0da501977b835238310db4d151d7
Artem <artembabuhkin@gmail.com>
2023-03-08 12:52:06 +0300

    Ввёл новый класс Book

--- Publishing/Publishing/Publishing.cpp
+++ Publishing/Publishing/Publishing.cpp
@@ -26,10 +26,10 @@ public:
 	}
 
 	void setYear() {
-		while ((Year < 1900) || (Year > 2023)) {	// 1900 > Year < Current year + 1
+		while ((Year < 1900) || (Year > 2023)) {	// 1900 > Year < 2023
 			cout << "Enter the year of publication:\n";
 			cin >> tmp_Year;
-			Year = atoi(tmp_Year); //Year is entered as a string, converted to int
+			Year = atoi(tmp_Year); //Year is entered as a char, converted to int
 		}
 	}
 
@@ -39,10 +39,67 @@ public:
 
 };
 
+class Book: public Publishing{
+private:
+	string ISBN,Publisher,Author;
+	int Quantity;
+	string tmp_Quantity;
+
+public:
+	void setISBN() {
+		while (!regex_match(ISBN, regex("([0-9]{1,5}[-]{0,1}){5}"))) {
+			cout << "Enter the ISBN number : \n";
+			getline(cin, ISBN);
+		}
+	}
+	string getISBN() {
+		return ISBN;
+	}
+
+
+	void setPublisher() {
+		while (!regex_match(Publisher, regex("[A-Z][a-z]{1,32}"))) {
+			cout << "Enter a Publisher:\n";
+			getline(cin, Publisher);
+		}
+	}
+	string getPublisher() {
+		return Author;
+	}
+
+	void setAuthor() {
+		while (!regex_match(Author, regex("[A-Z][a-z]{1,32}"))) {
+			cout << "Enter a Author:\n";
+			getline(cin, Author);
+		}
+	}
+	string getAuthor() {
+		return Author;
+	}
+
+
+	void setQuantity() {															//Quantity has no quantitative limits, so you cannot use the same method as for Year. 
+		while (!regex_match(tmp_Quantity, regex("[0-9]{1,10}"))) {					//stoi() does not return 0 like atoi(), so a try/catch construct is introduced
+			cout << "Enter a Quantity:\n";
+			getline(cin, tmp_Quantity);
+			try {
+				Quantity = stoi(tmp_Quantity); //Quantity is entered as a string, converted to int
+			}
+			catch (invalid_argument) {
+				cout << "Please, enter an integer\n";
+			}
+		}
+	}
+
+	int getQuantity() {
+		return Quantity;
+	}
+};
+
 int main() {  							//!!!DO AN ISBN CHECK!!!
 	locale::global(std::locale(""));
-	Publishing test;
-	test.setName();
-	cout << test.getName();
+	Book test;
+	test.setQuantity();
+	cout << test.getQuantity();
 	return 0;
 }
